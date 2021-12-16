40ee320e2a65122ec0560f141ad5dda5b81189b0
newbazz <krutamhathi14@gmail.com>
2021-12-16 19:09:08 +0530

    experiment pushed

---
+++ public/image_compress.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from PIL import Image
+
+def convert_to_webp(source):
+    destination = source.with_suffix(".webp")
+
+    image = Image.open(source)
+    image.save(destination, format="webp", optimize = True, quality = 10)
+
+    return destination
+
+
+def main():
+    paths = Path("./assets/images").glob("**/*.jpg")
+    for path in paths:
+        webp_path = convert_to_webp(path)
+        print(webp_path)
+
+
+main()
